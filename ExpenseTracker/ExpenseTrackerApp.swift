1c2f3eaab2c580a26493655d52738053d97d11c2
Rolva Mashale <mashalerolva@gmail.com>
2023-09-08 12:37:20 +0200

    created an extension folder and declared colors  that will be used in the background, text and icon

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by DA MAC  M1 154 on 2023/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTracker/Extentions.swift
@@ -0,0 +1,19 @@
+//
+//  Extentions.swift
+//  ExpenseTracker
+//
+//  Created by DA MAC  M1 154 on 2023/09/08.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let  background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+    
+    
+}
